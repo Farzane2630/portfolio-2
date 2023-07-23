9bd6a2fe9c9d7d28a90610875fa689f9361b4811
Farzaneh <farzanekazemi8517@gmail.com>
2023-07-23 18:36:45 +0330

    style: change styles for service-box

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
-import { useRoutes } from 'react-router-dom';
+import { useLocation, useRoutes } from 'react-router-dom';
 import routes from './routes'
 import Navigationbar from './components/Navbar/Navbar';
 import Footer from './Footer/Footer/Footer'
+import { useEffect } from 'react';
 function App() {
+
+  const { pathname } = useLocation();
+
+  useEffect(()=>{
+    window.scrollTo(0, 0)
+  },[pathname])
 
   let router = useRoutes(routes)
   return (
--- src/components/services/Services/Services.js
+++ src/components/services/Services/Services.js
@@ -6,10 +6,10 @@ export default class Services extends React.Component {
 
   state = {
     imgArray : [
-      {id:1, image: "https://preview.colorlib.com/theme/calvino/assets/img/icon/services1.svg", title: "Discover, Explore the Product1", description: "Effective strategies to help you reach customers.1"},
-      {id:2, image: "https://preview.colorlib.com/theme/calvino/assets/img/icon/services2.svg", title: "Discover, Explore the Product2", description: "Effective strategies to help you reach customers.2"},
-      {id:3, image: "https://preview.colorlib.com/theme/calvino/assets/img/icon/services3.svg", title: "Discover, Explore the Product3", description: "Effective strategies to help you reach customers.3"},
-      {id:4, image: "https://preview.colorlib.com/theme/calvino/assets/img/icon/services4.svg", title: "Discover, Explore the Product4", description: "Effective strategies to help you reach customers.4"},
+      {id:1, image: "https://preview.colorlib.com/theme/calvino/assets/img/icon/services1.svg", title: "Discover, Explore the Product #1", description: "Effective strategies to help you reach customers.1"},
+      {id:2, image: "https://preview.colorlib.com/theme/calvino/assets/img/icon/services2.svg", title: "Discover, Explore the Product #2", description: "Effective strategies to help you reach customers.2"},
+      {id:3, image: "https://preview.colorlib.com/theme/calvino/assets/img/icon/services3.svg", title: "Discover, Explore the Product #3", description: "Effective strategies to help you reach customers.3"},
+      {id:4, image: "https://preview.colorlib.com/theme/calvino/assets/img/icon/services4.svg", title: "Discover, Explore the Product #4", description: "Effective strategies to help you reach customers.4"},
     ]
   }
   render () {
